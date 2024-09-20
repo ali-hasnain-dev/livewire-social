cd4b0233dc7bd36ff1a5f4ce4b27d6d58a8e21c6
unknown <alihasnainhfd@gmail.com>
2024-09-20 19:08:51 +0500

    email verification

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Livewire\Pages\Auth\Forgetpassword;
 use App\Livewire\Pages\Auth\ResetPassword;
 use App\Livewire\Pages\Auth\Signin;
 use App\Livewire\Pages\Auth\Signup;
+use App\Livewire\Pages\Auth\VerifyEmail;
 use App\Livewire\Pages\Home;
 use App\Livewire\Pages\Profile;
 use App\Livewire\Pages\Settings;
@@ -18,7 +19,11 @@ Route::get('/signup', Signup::class)->name('signup');
 Route::get('/forgot-password', Forgetpassword::class)->name('forgot-password');
 Route::get('reset-password/{token}', ResetPassword::class)->name('password.reset');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware('auth')->group(function () {
+    Route::get('verify-email', VerifyEmail::class)->name('verification.notice');
+});
+
+Route::middleware(['auth'])->group(function () {
     Route::get('/', Home::class)->name('home');
     Route::get('/settings', Settings::class)->name('Settings');
     Route::get('/profile/{name?}', Profile::class)->name('profile');
