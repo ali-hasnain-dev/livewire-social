af975553d6ebdaf3944fafd851a8c348040a56a8
unknown <alihasnainhfd@gmail.com>
2024-09-21 12:45:11 +0500

    verfication email

--- app/Livewire/Pages/Auth/VerifyEmail.php
+++ app/Livewire/Pages/Auth/VerifyEmail.php
@@ -2,10 +2,36 @@
 
 namespace App\Livewire\Pages\Auth;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Livewire\Component;
 
 class VerifyEmail extends Component
 {
+    public function sendVerification(): void
+    {
+
+        if (Auth::user()->hasVerifiedEmail()) {
+            $this->redirectIntended(default: route('dashboard', absolute: false), navigate: true);
+
+            return;
+        }
+
+        Auth::user()->sendEmailVerificationNotification();
+
+        Session::flash('status', 'verification-link-sent');
+    }
+
+    /**
+     * Log the current user out of the application.
+     */
+    public function logout(Logout $logout): void
+    {
+        $logout();
+
+        $this->redirect('/', navigate: true);
+    }
+
     public function render()
     {
         return view('livewire.pages.auth.verify-email');
--- resources/views/livewire/pages/auth/verify-email.blade.php
+++ resources/views/livewire/pages/auth/verify-email.blade.php
@@ -1,5 +1,16 @@
-<div>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+<div class="mt-24 flex items-center justify-center">
+    <div class="flex flex-col gap-4 w-full md:w-[400px] bg-white p-8 rounded-md">
+        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        </div>
+
+        <div class="mt-4 flex items-center justify-between">
+            <button class="p-2 bg-blue-500 text-white rounded-md" wire:click="sendVerificationEmail">Resend Verification
+                Email</button>
+            <button wire:click="logout" type="submit"
+                class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                Logout
+            </button>
+        </div>
     </div>
 </div>
