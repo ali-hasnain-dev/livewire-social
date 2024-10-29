861c94a64b4a66944519fc99e497bd21be7ca7ab
unknown <alihasnainhfd@gmail.com>
2024-10-29 10:21:13 +0500

    write command for livewire temp file cleared

---
+++ app/Console/Commands/ClearLivewireTemp.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Console\Command;
+
+class ClearLivewireTemp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:clear-livewire-temp';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear the livewire-temp directory';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $path = storage_path('app\livewire-tmp');
+
+        if (is_dir($path)) {
+            File::cleanDirectory($path);
+            $this->info('Livewire-temp directory cleared.');
+        } else {
+            $this->error("The specified path does not exist or is not a directory: $path");
+        }
+
+        return 0;
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('clear-livewire-temp', function () {
+    (new \App\Console\Commands\ClearLivewireTemp())->handle();
+})->purpose('Clear Livewire temp directory')->dailyAt('00:00');
